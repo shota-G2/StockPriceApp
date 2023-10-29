88ff7ba951d875f04502ae9a5dc7735eb125ddcf
shota-G2 <marimocag2@gmail.com>
2023-10-29 21:52:54 +0900

    端末依存のレイアウト修正

--- app/src/main/java/com/example/stockpriceapp/SerchScreen.kt
+++ app/src/main/java/com/example/stockpriceapp/SerchScreen.kt
@@ -1,40 +1,32 @@
 package com.example.stockpriceapp
 
 import android.annotation.SuppressLint
-import android.inputmethodservice.Keyboard
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,17 +35,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -84,51 +72,24 @@ fun SerchScreen(navController: NavController){
                     .fillMaxSize()
             ) {
                 val displayList by remember { mutableStateOf(activeCompanyName) }
-                TopBar("検索")
-//                SerchBar(displayList)
-                SerchList(navController, displayList)
-                MainMenu(navController)
+                Column {
+                    TopBar("検索")
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ){
+                    SerchList(navController, displayList)
+                }
+                Column(
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    MainMenu(navController)
+                }
             }
         }
     }
 }
-
-//@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-//@Composable
-//fun SerchBar(displayList: MutableList<String>) {
-//    val keyboardController = LocalSoftwareKeyboardController.current
-//    Box(modifier = Modifier
-//        .fillMaxWidth()
-//        .height(60.dp)
-//    ) {
-//        var text by remember { mutableStateOf("" ) }
-//        TextField(
-//            value = text,
-//            onValueChange = { text = it },
-//            colors = TextFieldDefaults.textFieldColors(),
-//            label = {
-//                Text(text = "ファンド名を入力",
-//                    color = Color.DarkGray
-//                )
-//            },
-//            modifier = Modifier
-//                .fillMaxWidth(),
-//            leadingIcon = { Icon(Icons.Filled.Search, null)},
-//            singleLine = true,
-//            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-//            keyboardActions = KeyboardActions(onSearch = {
-//                displayList.clear()
-//                for (i in 0 until activeCompanyName.size){
-//                    if (activeCompanyName.contains(text)){
-//                        displayList.add(activeCompanyName[i])
-//                    }
-//                }
-//                keyboardController?.hide()
-//            })
-//        )
-//    }
-//}
-
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
--- app/src/main/java/com/example/stockpriceapp/WatchList.kt
+++ app/src/main/java/com/example/stockpriceapp/WatchList.kt
@@ -1,9 +1,6 @@
 package com.example.stockpriceapp
 
 import android.os.Build
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -16,29 +13,32 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -46,19 +46,10 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.stockpriceapp.ui.theme.StockPriceAppTheme
-import com.github.kittinunf.fuel.Fuel
-import com.google.android.gms.cast.RequestData
-import io.realm.Realm
-import io.realm.kotlin.where
-import kotlinx.coroutines.runBlocking
+import java.util.Collections
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -69,11 +60,25 @@ fun WatchListScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(modifier = Modifier.background(Color.Black)
+            Column(
+                modifier = Modifier
+                    .background(Color.Black)
+                    .fillMaxSize()
             ) {
-                TopBar("ウォッチリスト")
-                WatchList(navController)
-                MainMenu(navController)
+                Column {
+                    TopBar("ウォッチリスト")
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ){
+                    WatchList(navController)
+                }
+                Column(
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    MainMenu(navController)
+                }
             }
         }
     }
@@ -94,105 +99,111 @@ fun TopBar(text: String){
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun WatchList(navController: NavController) {
-
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Gray)
-            .horizontalScroll(rememberScrollState())
+            .fillMaxSize()
     ) {
-        for (i in 0..5){
-            Button(
-                onClick = {  },
-                colors = ButtonDefaults.buttonColors(Color.Gray)
-            ) {
-                Text("ウォッチリスト$i")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray)
+                .horizontalScroll(rememberScrollState())
+        ) {
+            for (i in 0..5){
+                Button(
+                    onClick = {  },
+                    colors = ButtonDefaults.buttonColors(Color.Gray)
+                ) {
+                    Text("ウォッチリスト$i")
+                }
             }
         }
-    }
 
-    val myApp = MyApp.getInstance()
-    val watchList = myApp.watchList
-    val watchListIndexClose = myApp.watchListIndexClose
-    val watchListDifference = myApp.watchListDifference
-    val referenceDate = myApp.referenceDate.replace("-", "/")
+        val myApp = MyApp.getInstance()
+        val watchList = myApp.watchList
+        val watchListIndexClose = myApp.watchListIndexClose
+        val watchListDifference = myApp.watchListDifference
+        val referenceDate = myApp.referenceDate.replace("-", "/")
 
-    Column(modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.End
-    ){
-        Text(text = "(基準日:$referenceDate)",
-            fontSize = 10.sp,
-            color = Color.White
-        )
-    }
 
-    LazyColumn(modifier = Modifier
-        .fillMaxWidth()
-        .size(630.dp)
-    ) {
-        itemsIndexed(watchList){ indexNum, watchList ->
-            val indexClose = watchListIndexClose[indexNum]
-            val difference = watchListDifference[indexNum]
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        navController.navigate("indexDetail/$watchList/$indexClose/$difference")
-                    }
-            ){
-                Box {
-                    Image(painter = painterResource(id = R.drawable.wine),
-                        contentDescription = null,
-                        contentScale = ContentScale.FillBounds,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 10.dp, start = 10.dp, end = 10.dp,)
-                            .size(65.dp)
-                    )
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End
+        ){
+            Text(text = "(基準日:$referenceDate)",
+                fontSize = 10.sp,
+                color = Color.White
+            )
+        }
 
-                    Column {
-                        Text(text = watchList,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            fontSize = 15.sp,
-                            color = Color.White,
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .size(630.dp)
+        ) {
+            itemsIndexed(watchList){ indexNum, watchList ->
+                val indexClose = watchListIndexClose[indexNum]
+                val difference = watchListDifference[indexNum]
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            navController.navigate("indexDetail/$watchList/$indexClose/$difference")
+                        }
+                ){
+                    Box {
+                        Image(painter = painterResource(id = R.drawable.wine),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillBounds,
                             modifier = Modifier
-                                .padding(start = 15.dp, top = 15.dp),
+                                .fillMaxWidth()
+                                .padding(top = 10.dp, start = 10.dp, end = 10.dp,)
+                                .size(65.dp)
                         )
-                        Row {
-                            Text(
-                                "前日終値",
-                                fontSize = 15.sp,
-                                color = Color.White,
-                                modifier = Modifier
-                                    .padding(start = 15.dp)
-                            )
 
-                            Text(
-                                watchListIndexClose[indexNum],
-                                fontSize = 20.sp,
-                                color = Color.White,
-                                modifier = Modifier
-                                    .padding(start = 10.dp)
-                            )
-                            Text(
-                                "前日比",
+                        Collections.replaceAll(watchListIndexClose, "null", "-")
+                        Column {
+                            Text(text = watchList,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
                                 fontSize = 15.sp,
                                 color = Color.White,
                                 modifier = Modifier
-                                    .padding(start = 30.dp)
+                                    .padding(start = 15.dp, top = 15.dp),
                             )
-                            Text(
-                                watchListDifference[indexNum],
-                                fontSize = 20.sp,
-                                color = if (watchListDifference[indexNum] != "-"){
-                                    if (watchListDifference[indexNum].toFloat() >= 0) {Color.Green } else {Color.Red}
-                                } else {
-                                    Color.White
-                                },
-                                modifier = Modifier
-                                    .padding(start = 10.dp)
-                            )
+                            Row {
+                                Text(
+                                    "前日終値",
+                                    fontSize = 15.sp,
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .padding(start = 15.dp)
+                                )
+
+                                Text(
+                                    watchListIndexClose[indexNum],
+                                    fontSize = 20.sp,
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .padding(start = 10.dp)
+                                )
+                                Text(
+                                    "前日比",
+                                    fontSize = 15.sp,
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .padding(start = 30.dp)
+                                )
+                                Text(
+                                    watchListDifference[indexNum],
+                                    fontSize = 20.sp,
+                                    color = if (watchListDifference[indexNum] != "-"){
+                                        if (watchListDifference[indexNum].toFloat() >= 0) {Color.Green } else {Color.Red}
+                                    } else {
+                                        Color.White
+                                    },
+                                    modifier = Modifier
+                                        .padding(start = 10.dp)
+                                )
+                            }
                         }
                     }
                 }
@@ -202,44 +213,88 @@ fun WatchList(navController: NavController) {
 }
 
 @Composable
-fun MainMenu(navController: NavController){
-    val fontSize = 15.sp
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .background(Color.Black)
-        .size(65.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
+fun MainMenu(navController: NavController) {
+    val color = Color.White
+    val iconSize = Modifier.size(30.dp)
+    val textSize = 15.sp
 
-        val textColor = Color.White
+    BottomAppBar(
+        modifier = Modifier
+            .height(80.dp)
+            .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)),
+        containerColor = Color.Gray,
 
-        TextButton(onClick = { navController.navigate("watchListScreen") },
-        ) {
-            Text("ウォッチリスト",
-                color = textColor,
-                fontSize = fontSize
-            )
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 30.dp, end = 30.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    IconButton(onClick = { navController.navigate("watchListScreen") }) {
+                        Icon(
+                            Icons.Filled.Favorite,
+                            contentDescription = "watchList",
+                            modifier = iconSize,
+                            tint = color
+                        )
+                    }
+                    IconButton(onClick = { navController.navigate("serchScreen") }) {
+                        Icon(
+                            Icons.Filled.Search,
+                            contentDescription = "search",
+                            modifier = iconSize,
+                            tint = color
+                        )
+                    }
+                    IconButton(onClick = { navController.navigate("loginScreen") }) {
+                        Icon(
+                            Icons.Filled.Home,
+                            contentDescription = "logout",
+                            modifier = iconSize,
+                            tint = color
+                        )
+                    }
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 20.dp, bottom = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,) {
+                    Text(
+                        "ウォッチリスト",
+                        color = color,
+                        fontSize = textSize
+                        )
+                    Text(
+                        "検索",
+                        color = color,
+                        fontSize = textSize
+                    )
+                    Text(
+                        "ログアウト",
+                        color = color,
+                        fontSize = textSize
+                    )
+                }
+            }
         }
-        TextButton(onClick = { navController.navigate("serchScreen") },
-        ) {
-            Text("検索",
-                color = textColor,
-                fontSize = fontSize
-            )
-        }
-        TextButton(onClick = { navController.navigate("loginScreen") },
-        ) {
-            Text("サインアウト",
-                color = textColor,
-                fontSize = fontSize
-            )
-        }
-        TextButton(onClick = { navController.navigateUp() },
-        ) {
-            Text("まなぶ",
-                color = textColor,
-                fontSize = fontSize
-            )
-        }
-    }
+    )
 }
+
+//@RequiresApi(Build.VERSION_CODES.O)
+//@Preview
+//@Composable
+//fun WatchScreenPreview(){
+//    val navController = rememberNavController()
+//    WatchListScreen(navController)
+//}
+//@Preview
+//@Composable
+//fun Preview(){
+//    val navController = rememberNavController()
+//    MainMenu(navController)
+//}
